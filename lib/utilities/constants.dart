1bbfbe510cdafefbc052d22fb7ecd5e5dc343e86
CharukaKavi1996 <charukarathnayaka1996@gmail.com>
2022-12-07 10:54:16 +0530

    asset constants

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,14 @@
+class Constants{
+
+  // assets paths
+  static const IMAGE_PATH = 'assets/images/';
+  static const ICON_PATH = 'assets/icons/';
+
+  //image asset function
+  static String imageAsset(img) => "$IMAGE_PATH$img";
+
+  //icon asset function
+   static String iconAsset(img) => "$ICON_PATH$img";
+
+
+}
